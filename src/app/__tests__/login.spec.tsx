383f155774bc8369a04ef6b838fc3b30555473d8
Gui-dev <adrianon10@gmail.com>
2025-05-16 03:42:45 -0300

    test: it should be able renders the form inputs

---
+++ src/app/__tests__/login.spec.tsx
@@ -0,0 +1,43 @@
+import { describe, expect, it, vi } from 'vitest'
+import { render, screen } from '@testing-library/react'
+
+import Home from '@/app/page'
+import { AuthContext } from '@/context/auth-provider'
+import { beforeEach } from 'node:test'
+import { useRouter } from 'next/navigation'
+
+vi.mock('next/navigation', () => ({
+  useRouter: vi.fn(),
+}))
+
+describe('<LoginPage />', () => {
+  const loginMock = vi.fn()
+  const pushMock = vi.fn()
+
+  beforeEach(() => {
+    vi.clearAllMocks()
+    vi.mocked(useRouter).mockReturnValue({ push: pushMock } as any)
+  })
+
+  const renderLoginPage = () => {
+    render(
+      <AuthContext.Provider
+        value={{
+          user: null,
+          login: loginMock,
+          logout: vi.fn(),
+        }}
+      >
+        <Home />
+      </AuthContext.Provider>
+    )
+  }
+
+  it('should be able renders the form inputs', async () => {
+    renderLoginPage()
+
+    expect(screen.getByPlaceholderText(/e-mail/i)).toBeInTheDocument()
+    expect(screen.getByPlaceholderText(/senha/i)).toBeInTheDocument()
+    expect(screen.getByRole('button', { name: /entrar/i })).toBeInTheDocument()
+  })
+})
